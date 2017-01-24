f5f6fd3be5b2fe284d8bb0783c54eaa736d59cc9
gactocat <gactocat@gmail.com>
2017-01-24 20:54:06 +0900

    add redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './main.css'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import reducer from './reducers'
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
